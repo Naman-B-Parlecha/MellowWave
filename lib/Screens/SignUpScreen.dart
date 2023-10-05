1eae16cecfe41d60e075852bf8e8c13265561eeb
Naman-Parlecha <namanparlecha@gmail.com>
2023-10-05 21:00:26 +0530

    updated SignUP UI/UX

--- lib/Screens/SignUpScreen.dart
+++ lib/Screens/SignUpScreen.dart
@@ -10,107 +10,245 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
-  int screennumber = 0;
+  int count = 0;
+  Widget? content;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        body: Stack(
-          children: [
-            Positioned(
-              top: 0,
-              right: 0,
-              left: 0,
-              child: Image.asset(
-                'assets/images/music.jpg',
-                height: 465,
-                fit: BoxFit.fill,
-              ),
+    if (count == 0) {
+      content = Stack(
+        children: [
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            child: Container(
+              width: double.infinity,
+              child: Image.asset('assets/images/back2.jpg'),
             ),
-            Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                  padding: const EdgeInsets.only(
-                      top: 50, left: 10, right: 10, bottom: 15),
-                  height: 450,
-                  decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(25),
-                          topLeft: Radius.circular(25)),
-                      color: Color(0xFF57837B)),
-                  child: Column(
-                    children: [
-                      Text(
-                        "Welcome to MellowWave",
-                        style: GoogleFonts.montserrat(
-                            color: Colors.black,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.only(top: 45, left: 20, right: 20),
+              width: double.infinity,
+              height: 400,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Welcome to MellowWave',
+                    style: GoogleFonts.montserrat(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 3),
+                  Text(
+                    'Relax and enjoy Lofi beats\neffortlessly.',
+                    style: GoogleFonts.montserrat(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 45),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        count = 1;
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
                       ),
-                      const SizedBox(height: 8),
-                      Text(
-                        "Relax and enjoy Lofi beats\neffortlessly.",
+                      padding: const EdgeInsets.all(16),
+                    ),
+                    child: SizedBox(
+                      width: 200,
+                      child: Text(
+                        'Sign Up',
                         style: GoogleFonts.montserrat(
-                            color: Colors.black,
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500),
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
                         textAlign: TextAlign.center,
                       ),
-                      const SizedBox(height: 70),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          padding: EdgeInsets.all(16.0),
+                    ),
+                  ),
+                  const SizedBox(height: 3),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Already have an account?',
+                        style: GoogleFonts.montserrat(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
                         ),
-                        onPressed: () {},
-                        child: Container(
-                          width: 250,
-                          alignment: Alignment.center,
-                          child: Text(
-                            'Sign Up',
-                            style: GoogleFonts.montserrat(
-                                color: const Color(0xFF515E63),
-                                fontSize: 16,
-                                fontWeight: FontWeight.w500),
+                        textAlign: TextAlign.center,
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          setState(() {
+                            count = 2;
+                          });
+                        },
+                        child: Text(
+                          'Sign In',
+                          style: GoogleFonts.montserrat(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
                           ),
                         ),
                       ),
-                      const SizedBox(height: 5),
-                      Container(
-                        margin: const EdgeInsets.only(left: 70),
-                        child: Row(
-                          children: [
-                            Text(
-                              "Already have an account?",
-                              style: GoogleFonts.montserrat(
-                                  color: Colors.black,
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w500),
-                              textAlign: TextAlign.center,
-                            ),
-                            TextButton(
-                                onPressed: () {},
-                                child: Text(
-                                  "Sign in",
-                                  style: GoogleFonts.montserrat(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w500,
-                                    decoration: TextDecoration.underline,
-                                    color: Colors.white,
-                                  ),
-                                ))
-                          ],
-                        ),
-                      )
                     ],
                   ),
-                ))
-          ],
-        ));
+                ],
+              ),
+            ),
+          ),
+        ],
+      );
+    } else if (count == 1) {
+      content = Stack(
+        children: [
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            child: Container(
+              width: double.infinity,
+              child: Image.asset('assets/images/back2.jpg'),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.only(top: 45, left: 20, right: 20),
+                width: double.infinity,
+                height: 500,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      'Create a new account',
+                      style: GoogleFonts.montserrat(
+                          fontSize: 25, fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 30),
+                    Container(
+                      height: 180,
+                      width: 300,
+                      child: SingleChildScrollView(
+                          child: Column(
+                        children: [
+                          TextField(
+                            decoration: InputDecoration(
+                              labelText: 'Email',
+                              hintText: 'Enter your email',
+                              labelStyle: TextStyle(color: Colors.black),
+                              hintStyle: TextStyle(color: Colors.black),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                          SizedBox(height: 20.0),
+                          TextField(
+                            decoration: InputDecoration(
+                              labelText: 'Password',
+                              hintText: 'Enter your password',
+                              labelStyle: TextStyle(color: Colors.black),
+                              hintStyle: TextStyle(color: Colors.black),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                            ),
+                            obscureText: true,
+                          ),
+                        ],
+                      )),
+                    ),
+                    SizedBox(height: 25),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        padding: const EdgeInsets.all(16),
+                      ),
+                      child: SizedBox(
+                        width: 200,
+                        child: Text(
+                          'Sign Up',
+                          style: GoogleFonts.montserrat(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Already have an account?',
+                          style: GoogleFonts.montserrat(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            setState(() {
+                              count = 2;
+                            });
+                          },
+                          child: Text(
+                            'Sign In',
+                            style: GoogleFonts.montserrat(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                )),
+          ),
+        ],
+      );
+    }
+
+    return Scaffold(body: content);
   }
 }
